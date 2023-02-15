6581611a57b2fd40e700c9f0b4e38bea322e0210
bkfan1 <jpf8296@gmail.com>
2023-02-14 20:13:54 -0400

    Refactored logic inside steam_category_offers_fetcher function

--- fetchers.py
+++ fetchers.py
@@ -1,19 +1,29 @@
 import requests
 
 
-def steam_offers_fetcher(category):
-    try:
-        res = requests.get(
-            'https://store.steampowered.com/api/featuredcategories')
-        data = res.json()
+def steam_category_offers_fetcher(category_name="", country_code='US'):
 
-        if category == 'specials':
-            specials = data['specials']['items']
-            return specials
+    FEATURED_CATEGORIES_URL = f'https://store.steampowered.com/api/featuredcategories?cc={country_code}'
+    res = requests.get(url=FEATURED_CATEGORIES_URL)
+    res_dict = res.json()
 
-        if category == 'top_sellers':
-            top_sellers = data['top_sellers']['items']
-            return top_sellers
-    except:
-        raise Exception(
-            'An error occurred while attempting to fetch offers data.')
+    category_offers_data = res_dict.get(category_name).get('items')
+
+    offers_list = []
+
+    for category_offer_data in category_offers_data:
+
+        app_id = str(category_offer_data.get('id'))
+
+        APP_DETAILS_URL = f'https://store.steampowered.com/api/appdetails/?appids={app_id}&cc={country_code}'
+        
+        res = requests.get(url=APP_DETAILS_URL)
+        res_dict = res.json()
+
+        success = res_dict.get(app_id).get('success')
+
+        if success == True:
+            app_data = res_dict.get(app_id).get('data')
+            offers_list.append(app_data)
+
+    return offers_list
